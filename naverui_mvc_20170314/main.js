8dd8480cd66f78c38fadf1baae1fab48b804b398
sujinleeme <sujinlee.me@gmail.com>
2017-03-15 17:29:27 +0900

    [dev_1.2] Create. today's coding practice

--- naverui_mvc_20170314/main.js
+++ naverui_mvc_20170314/main.js
@@ -1,3 +1,20 @@
+var healthObj = {
+    name: "달리기",
+    lastTime: "PM10:12",
+    showHealth: function(){
+        console.log("오늘은"+ this.lastTime+"까지"+this.name+"하셨어요.");
+    }
+}
+
+healthObj.showHealth();
+
+function Health(name, lastTime){
+    this.name = name;
+    this.lastTime = lastTime;
+};
+
+
+Health.prototype = healthObj;
 
 
 
@@ -9,7 +26,8 @@
         contentBox : document.querySelector(".content"),
         leftButton : document.querySelector(".left"),
         rightButton : document.querySelector(".right"),
-        companyList : document.querySelector("nav").querySelector("ul").children,
+        companyList : document.querySelector("nav > ul").children,
+        companyName: document.querySelector(".newsName")
     };
 
     var url = 'data/newslist.json';
@@ -31,34 +49,41 @@
     function init(data){
         DOM.sideNav.insertAdjacentHTML('beforebegin', "<span><strong></strong>/"+data.length+"</span>");
         for (var i in data) {
-            companyName = data[i].title ;
-            DOM.sideNav.insertAdjacentHTML('beforeend', "<li>"+companyName+"</li>");
+            DOM.sideNav.insertAdjacentHTML('beforeend', "<li>"+data[i].title+"</li>");
         };
-        showArticleContainer(0, data);
+        setArticleContainerHTML(0, data);
     }
-    var index = 0;
+
     function evtn(data){
-        
-        for(var i=0; i<DOM.companyList.length; i++){
+        for(let i=0; i<DOM.companyList.length; i++){
             DOM.companyList[i].addEventListener("click", function(evt) {
-                index = [].indexOf.call (this.parentNode.children, this);
-                showArticleContainer(index, data)                
+                title = DOM.companyList[i].innerHTML;
+                index = getNewIndexNum(title, data);
+                setArticleContainerHTML(index, data)                
 		    });
         }
 
         DOM.rightButton.addEventListener("click", function(evt){
-            index++;
-            if (index > data.length-1){ index = data.length-1;}
-            showArticleContainer(index, data); 
+            newsName = document.querySelector(".newsName").innerHTML;
+            newsIndex = getNewIndexNum(newsName, data);
+            setArticleContainerHTML(newsIndex+1, data); 
         });
 
         DOM.leftButton.addEventListener("click", function(evt){
-            if (index < 1){
-                index = 1;
-            }
-            index--;
-            showArticleContainer(index, data);
+            newsName = document.querySelector(".newsName").innerHTML;
+            newsIndex = getNewIndexNum(newsName, data);
+            setArticleContainerHTML(newsIndex-1, data);
         });
+          // function showArticleContainer(index, data){
+        //setArticleContainerHTML(index, data);
+       
+    
+
+    }
+
+    function getNewIndexNum(companyName, data){
+        itemIndex = data.findIndex(x => x.title==companyName);
+        return itemIndex;
     }
     
     function deleteCompanyList(index, data){
@@ -66,45 +91,41 @@
         setArticleContainerHTML(index, data);
     }
 
-
-    function showArticleContainer(index, data){
-        
-        setArticleContainerHTML(index, data);
-
-        var closeButton = DOM.contentBox.querySelector('button');
-        closeButton.addEventListener("click", function(evt){
-            target = evt.target;
-            if (target.tagName === "A"){
-                target = target.parentNode;
-            }
-            console.log(target);
-            companyName = DOM.companyList[index].innerHTML;
-        
-            deleteCompanyList(index, data);
-            
-            
-        });
-    }
-
     function setArticleContainerHTML(index, data){
-
         DOM.mainArea.querySelector("strong").innerHTML = index+1;
         companyName = DOM.companyList[index].innerHTML;
         itemIndex = data.findIndex(x => x.title==companyName);
-
-        Newstemplate = document.querySelector("#newsTemplate").innerHTML;
-        Newstemplate = Newstemplate.replace("{title}", data[itemIndex].title);
-        Newstemplate = Newstemplate.replace("{imgurl}", data[itemIndex].imgurl);
+        newsTemplate = document.querySelector("#newsTemplate").innerHTML;
         articleHTML = '';
         articleData = data[itemIndex].newslist;
         for(var i=0; i<articleData.length; i++){
             articleHTML += "<li>"+articleData[i]+"</li>"
         }
-        Newstemplate = Newstemplate.replace("{newsList}", articleHTML);
-        DOM.contentBox.innerHTML = Newstemplate;
+        newsTemplate = newsTemplate.replace("{title}", data[itemIndex].title).replace("{imgurl}", data[itemIndex].imgurl).replace("{newsList}", articleHTML);
+        DOM.contentBox.innerHTML = newsTemplate;
+
+
+        var closeButton = DOM.contentBox.querySelector('button');
+        console.log(closeButton);
+        // closeButton.addEventListener("click", function(evt){
+        //     target = evt.target;
+        //     if (target.tagName === "A"){ target = target.parentNode; }
+        //     companyName = DOM.companyList[index].innerHTML;
+        //     deleteCompanyList(index, data); 
+        // });
     }
-
-
-
-
 })();
+
+
+// (function(doc, win) {
+//     console.log(doc.body);
+//     var div = doc.querySelector("div");
+//     console.log(div);
+// }) (document, window);
+// 
+
+// arrowLeft.addEventListener("click", function(){
+//     moveLR(jsonObj, "L", currentIdx); //이벤트 큐에 저장된다.
+// });
+
+// arrowLeft.addEventListener("click", moveLR.bind(null, jsonObj, "L", curreniDX))

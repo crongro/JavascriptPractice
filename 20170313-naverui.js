4c5bc8f0e1fd88383287616eacdf2fee3fec16cb
sujinleeme <sujinlee.me@gmail.com>
2017-03-15 17:50:11 +0900

    [dev_1.2] Delete. .DS_Store

--- 20170313-naverui.js
+++ 20170313-naverui.js
@@ -1,16 +1,21 @@
+2015.03.14
 
+### 1. DOM className을 정규표현식을 사용
+
+* 조건1 : "-"가 들어가 있는 클래스
+* 조건2 : "_"가 들어가 있는 클래스
+
+```javascript
 var sectionList = document.querySelectorAll("div");
 
 function chkClassDashStr(lst){
     var count = 0;
     for (var i = 0; i < lst.length; i++){
-        
         str = lst[i].className;
         if ( /[-_]/g.test(str)){
             lst[i].className = deleteClassStr(str);
             count++;
         }
-
     }
     return count;
 }
@@ -20,3 +25,9 @@ chkClassDashStr(sectionList);
 function deleteClassStr(){
     return '';
 }
+```
+
+```javascript
+const className = 'scrollbar-v scrollbar-show scrollbar-disabled goodDash badCamell forExample1'; // tag.className;
+console.log(className.match(/[^\s]*[-_][^\s]*(?=\s|$)/g));
+```

843c741e3962185aa51430b726d226d5c3f02d13
sujinleeme <sujinlee.me@gmail.com>
2017-03-17 19:50:02 +0900

    [dev_1.2] Update. OOP temp.js

--- 20170314-naverui_mvc/temp.js
+++ 20170314-naverui_mvc/temp.js
@@ -1,50 +1,69 @@
 //constructor
-// function BaseObj(title, imgurl, newlist, subscribe){
-//     this.title = title;
-//     this.imgurl = imgurl;
-//     this.newlist = [];
-//     this.subscribe = subscribe;
-// }
-
-
-
-var utilObj = {
-    runAjax : function(url){
+var utility = {
+    runAjax : function(url, listener, reqFunc){
         var oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", function(){
-             data = JSON.parse(this.responseText);
-        });
+        oReq.addEventListener(listener, reqFunc);
+
+        //함수실행
         oReq.open("GET", url);
         oReq.send();
-        
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log(utilObj.runAjax('data/newslist.json'));
-
-});
-
-var model = {
+ function main(){
+    var json = JSON.parse(this.responseText);
+    // 구독 key / val 추가
+    model._createNewKeyVal("isSubscribe", true, json);
+    
+    a = view._showSubscription();
+    view._showNavCompanyList(a);
+   // console.log(controller.join(model.init,view));
 
 }
 
-// 삭제 시  model.controller.isSubscrbe
-// 
 
+document.addEventListener("DOMContentLoaded", function() {
+    utility.runAjax("data/newslist.json", "load", main);
+});
 
+    
+var model = {
+    init: [],
+    _createNewKeyVal: function(key, val, data){
+        for (let i=0; i<data.length; i++){
+            data[i][key] = val;
+            data[i].id = i;
+            model.init.push(data[i]);
+        }
+    },
+}
 
 
 var view = {
+    join : function(model) {
+        this.model = model;
+    },
+
+    requestPageInfo: function(){
+
+    },
+
     showPageNum: function(){
         //페이지 번호 1/2
     },
 
-     showContent: function(){
+    showContent: function(){
        
      },
-     showNavCompany: function(){
-          //subscribedPress리스트 보여줌
+     _showNavCompanyList: function(data){
+        template = document.querySelector("#companyListTemplate").innerHTML;
+        container = document.querySelector(".mainArea > nav");
+        contentHTML = '';
+        for(var i=0; i<data.length; i++){
+            contentHTML += "<li>"+data[i].title+"</li>"
+        }
+        template = template.replace("{companyList}", contentHTML);
+        container.innerHTML = template;
      },
 
      hideContent : function(){
@@ -54,17 +73,14 @@ var view = {
 
      hideNavCompany : function(){
 
-     }
-     
+     },
+
+     //구독한 리스트
+     _showSubscription: function(){
+         return controller._filter("isSubscribe", true, model.init);
+     }     
 }
 
-
-var controller = function(model){
-// //     this.model = model 
-//         return this;
-// // }
-}
-// newObject
 var controller = {
     join : function(model, view) {
         this.model = model;
@@ -79,15 +95,26 @@ var controller = {
         //해당 객체 회사명을 조회해 index값만 반환
     },
 
-    isSubscrbe: function(){
+    subscribedPress: function(){
      // 클릭시 false로 바꿈
 
     },
 
     subscribedPress: function(){
-         //filter = false인 객체만 조회
-     }
+        
+    },
+
+    _filter: function(key, val, data){
+        var result = data.filter(function (el) {
+            return el[key] === val
+        });
+        return result? result : null;
+    }
 }
 
-// controller.join(model,view);
+// var result = allData.filter(function( obj ) {
+//         return obj.isSubscribe == true;
+//     });
+//     console.log(result);
+// 
 
